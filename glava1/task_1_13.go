dfdca2d239780574d2fca8f2a865336c18e331d9
cataclysm78 <42520898+moshkof@users.noreply.github.com>
2021-03-30 23:50:08 +0300

    commit7

--- glava1/task_1_13.go
+++ glava1/task_1_13.go
@@ -2,6 +2,11 @@ package glava1
 
 import "fmt"
 
-func main() {
-	fmt.Printf("")
+func summa() {
+	var x int
+	// var y int
+	// var z int
+	fmt.Scan(&x)
+	fmt.Println("Следующее за числом " + fmt.Sprint(x) + "число " + fmt.Sprint((x + 1)))
+
 }
